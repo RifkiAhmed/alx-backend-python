5476df07adaaf726a2587a785710dacc23012a0e
ahmed <rifkiahmed.91@gmail.com>
2024-01-11 13:23:29 +0100

    Concatenate two strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''Concatenation of two strings using type-annotated function'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''Return the sum of two numbers of type float'''
+    return str1 + str2
+
+# str1 = "egg"
+# str2 = "shell"
+
+# print(concat(str1, str2) == "{}{}".format(str1, str2))
+# print(concat.__annotations__)
