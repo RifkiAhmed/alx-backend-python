ccff53e7460f50e3fe2d102962d2c51a3d99002c
ahmed <rifkiahmed.91@gmail.com>
2024-01-11 17:16:37 +0100

    Add parameters and return values annotation to a given function

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-'''Annotate the function'''
+'''Annotate the parameters and the return values of the given function'''
 from typing import Iterable, List, Tuple, Sequence
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-    ''''''
+    '''return a list of tuples of each ele of the iterable lst and its len'''
     return [(i, len(i)) for i in lst]
 
 
