8ff040b08c32160be37e9cd5f7304261570800fd
ahmed <rifkiahmed.91@gmail.com>
2024-01-11 14:11:57 +0100

    Return sum of a mixed list of intgers and float elements

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -12,4 +12,5 @@ def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
 # mixed = [5, 4, 3.14, 666, 0.99]
 # ans = sum_mixed_list(mixed)
 # print(ans == sum(mixed))
-# print("sum_mixed_list(mixed) returns {} which is a {}".format(ans, type(ans)))
+# print("sum_mixed_list(mixed) returns {} which is a {}".format(
+#     ans, type(ans)))
