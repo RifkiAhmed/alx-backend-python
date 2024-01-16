1e89b07ebf59c905343c9d2b6a0a462fa7b9b61c
ahmed <rifkiahmed.91@gmail.com>
2024-01-16 17:12:00 +0100

    Define an asynchronous generator function

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+'''Asynchronous Generator'''
+import asyncio
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    '''
+    Asynchronous generator function that yields random numbers
+    between 0 and 10 with a 1s delay between each yield
+    '''
+    import random
+
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
+
+
+# Test case:
+#
+async def print_yielded_values():
+    result = []
+    async for i in async_generator():
+        result.append(i)
+    print(result)
+asyncio.run(print_yielded_values())
