a1fbb89c4f85377a39c0f153b62c6a88a88faf76
ahmed <rifkiahmed.91@gmail.com>
2024-02-02 17:36:25 +0100

    KeyError test cases for access_nested_map method

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -20,3 +20,12 @@ class TestAccessNestedMap(unittest.TestCase):
                                path: Sequence, expected: Any) -> None:
         '''Test that access_nested_map method returns what it is supposed to'''
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map: Mapping,
+                                         path: Sequence) -> None:
+        '''Test KeyError cases for access_nested_map method'''
+        self.assertRaises(KeyError, access_nested_map, nested_map, path)
