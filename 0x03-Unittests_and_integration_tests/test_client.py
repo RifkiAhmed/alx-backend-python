608be68775cc43d2eadac6a4a5202200c683436f
ahmed <rifkiahmed.91@gmail.com>
2024-02-03 17:37:53 +0100

    Test that public_repos returns what it is supposed to

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 '''Test module for org method'''
 from parameterized import parameterized
-from unittest.mock import patch, PropertyMock
+from unittest.mock import MagicMock, patch, PropertyMock
 import unittest
 
 GithubOrgClient = __import__('client').GithubOrgClient
@@ -35,25 +35,23 @@ class TestGithubOrgClient(unittest.TestCase):
             self.assertEqual(repos_url, url)
 
     @patch('client.get_json')
-    def test_public_repos(self, mock_get_json):
+    def test_public_repos(self, mock_get_json: MagicMock) -> None:
         '''Test that public_repos returns what it is supposed to'''
         repos = {
-            'repo1': {'name': 'alx-interview',
-                      "license": {"key": "your_license"}},
-            'repo2': {'name': 'alx-backend_python',
-                      "license": {"key": "my_license"}},
-            'repo3': {'name': 'alx-backend_storage',
-                      "license": {"key": "your_license"}},
-            'repo4': {'name': 'alx-backend_javascript',
-                      "license": {"key": "your_license"}}
+            'repo': [
+                {
+                    'name': 'alx-interview',
+                    "license": {"key": "my_license"}
+                    }
+                ]
         }
-        mock_get_json.return_value = repos
+        mock_get_json.return_value = repos['repo']
         with patch('client.GithubOrgClient._public_repos_url',
                    new_callable=PropertyMock) as mock_public_repos_url:
             url = 'https://api.github.com/orgs/abc'
             mock_public_repos_url.return_value = url
             client = GithubOrgClient('abc')
-            public_repos = client.public_repos(license='my_license')
-            self.assertEqual(public_repos, ['alx-backend_python'])
+            public_repos = client.public_repos()
+            self.assertEqual(public_repos, ['alx-interview'])
             mock_public_repos_url.assert_called_once()
         mock_get_json.assert_called_once()
